e0d15cdbbaf5e90947e65f5620a6f34f0324549a
Karla Grajales <k.grajalesluna@gmail.com>
2023-02-09 15:34:41 +0000

    Exercise done

--- exercises/fetch-exercise/exercise.js
+++ exercises/fetch-exercise/exercise.js
@@ -10,17 +10,24 @@ https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
 
 ================
-Expected result
+Expected result   
+"https://holy-snow-6805.fly.dev/api/greetings"
 ================
-
+.
 Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
 
-fetch("*** Write the API address here ***")
+fetch("https://holy-snow-6805.fly.dev/api/greetings")
   .then(function (response) {
-    return response.text();
+    return response.json();
   })
-  .then(function (greeting) {
-    // Write the code to display the greeting text here
+  .then(function (sayHello) {
+    let resultOfFetch = document.getElementById("greeting-text");
+    resultOfFetch.innerText = `language: ${sayHello.language} \n Greetings: ${sayHello.hello}`;
   });
+
+/*
+//".then()" is doing working similar like "await"
+//json will translate the information,
+//  and return in a lengauage that we can read*/
